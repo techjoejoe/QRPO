7ef1e01d020c24449473c34999bc77244b5aa1c1
techjoejoe <joe_o@mac.com>
2023-10-23 21:18:43 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from qr_points_app import app, socketio
 
 if __name__ == '__main__':
     print("Running the app with SocketIO...")
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=False)
